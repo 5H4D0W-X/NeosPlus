1628de869bc4076b4b31878e9c7d17453b8c2060
5H4D0W-X <99607717+5H4D0W-X@users.noreply.github.com>
2023-07-20 21:41:24 +0200

    Update RoundBevelPanelMesh.cs

--- NEOSPlus/Components/Assets/RoundBevelPanelMesh.cs
+++ NEOSPlus/Components/Assets/RoundBevelPanelMesh.cs
@@ -1,4 +1,4 @@
-﻿using BaseX;
+using BaseX;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +12,16 @@ namespace FrooxEngine
     {
         [Range(1, 64)] public readonly Sync<int> Sides;
         public readonly Sync<float2> Size;
-        [Range(0.01f, 0.1f)]public readonly Sync<float> Thickness;
+        [Range(0.01f, 0.1f)] public readonly Sync<float> Thickness;
+        [Range(0.01f, 0.5f)] public readonly Sync<float> CornerSize;
+        [Range(0.01f, 0.5f)] public readonly Sync<float> EdgeBevel;
 
         private RoundBevelPanel panel;
         private int _sides;
         private float2 _size;
         private float _thickness;
+        private float _primarybevel;
+        private float _secondarybevel;
 
         protected override void OnAwake()
         {
@@ -25,6 +29,8 @@ namespace FrooxEngine
             Sides.Value = 16;
             Size.Value = new float2(0.5f, 0.5f);
             Thickness.Value = 0.025f;
+            CornerSize.Value = 0.1f;
+            EdgeBevel.Value = 0.01f;
         }
 
         protected override void PrepareAssetUpdateData()
@@ -32,6 +38,8 @@ namespace FrooxEngine
             _sides = Sides.Value;
             _size = Size.Value;
             _thickness = Thickness.Value;
+            _primarybevel = CornerSize.Value;
+            _secondarybevel = EdgeBevel.Value;
         }
 
         protected override void ClearMeshData()
@@ -42,18 +50,18 @@ namespace FrooxEngine
         protected override void UpdateMeshData(MeshX meshx)
         {
             bool check = false;
-            if (panel == null || panel.Size != _size || panel.Thickness != _thickness)
+            if (panel == null || panel.Size != _size || panel.Thickness != _thickness || panel.Iterations != _sides || _primarybevel != panel.PrimaryBevel || _secondarybevel != panel.SecondaryBevel)
             {
                 panel?.Remove();
-                panel = new RoundBevelPanel(meshx, _size, _thickness, 0.1f, 0.01f, _sides);
+                panel = new RoundBevelPanel(meshx, _size, _thickness, _primarybevel, _secondarybevel, _sides);
                 check = true;
             }
 
             panel.Size = _size;
             panel.Thickness = _thickness;
             panel.Iterations = _sides;
-            panel.PrimaryBevel = 0.1f;
-            panel.SecondaryBevel = 0.01f;
+            panel.PrimaryBevel = _primarybevel;
+            panel.SecondaryBevel = _secondarybevel;
             panel.Update();
             uploadHint[MeshUploadHint.Flag.Geometry] = check;
         }
